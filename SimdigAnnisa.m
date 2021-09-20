fdb0f7ab78c4b700faae08f0f95ab5b8b445fe3c
Annisa Fitriani <71880105+Annisa190516@users.noreply.github.com>
2021-09-20 07:34:33 +0700

    Add files via upload
    Tugas 1 Simdig

---
+++ SimdigAnnisa.m
@@ -0,0 +1,133 @@
+function varargout = SimdigAnnisa(varargin)
+% SIMDIGANNISA MATLAB code for SimdigAnnisa.fig
+%      SIMDIGANNISA, by itself, creates a new SIMDIGANNISA or raises the existing
+%      singleton*.
+%
+%      H = SIMDIGANNISA returns the handle to a new SIMDIGANNISA or the handle to
+%      the existing singleton*.
+%
+%      SIMDIGANNISA('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SIMDIGANNISA.M with the given input arguments.
+%
+%      SIMDIGANNISA('Property','Value',...) creates a new SIMDIGANNISA or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before SimdigAnnisa_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to SimdigAnnisa_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help SimdigAnnisa
+
+% Last Modified by GUIDE v2.5 18-Sep-2021 21:50:06
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @SimdigAnnisa_OpeningFcn, ...
+                   'gui_OutputFcn',  @SimdigAnnisa_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before SimdigAnnisa is made visible.
+function SimdigAnnisa_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to SimdigAnnisa (see VARARGIN)
+
+% Choose default command line output for SimdigAnnisa
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes SimdigAnnisa wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = SimdigAnnisa_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+st = 10e-3;
+spp = 10;
+A = 220;
+f = 50;
+t = 2;
+n =0;
+phase = 0;
+bias = 0;
+for k = 0:st:t
+    n = n+1; 
+    O(n) = A*sin(f*k+phase)+bias;
+    wt(n) = k;
+end
+axes(handles.axes1)
+plot(wt,O)
+xlabel('waktu(detik)')
+ylabel('Amlitudo (Volt)')
+grid on
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+st = 10e-3;
+spp = 10;
+A = 220;
+f = 50;
+t = 2;
+n =0;
+G = 2;
+phase = 0;
+bias = 0;
+for k = 0:st:t
+    n = n+1;
+    O(n) = A*sin(f*k+phase)+bias;
+    O1(n) = G*O(n);
+    wt(n) = k;
+end
+axes(handles.axes2)
+plot(wt,O,'m',wt,O1,'y')
+xlabel('waktu(detik)')
+ylabel('Amlitudo (Volt)')
+grid on
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+delete(handles.figure1)
